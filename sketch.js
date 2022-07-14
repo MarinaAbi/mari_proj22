a6158c3c6de10f1930517ce371e3ed23ebbf131a
MarinaAbi <101902222+MarinaAbi@users.noreply.github.com>
2022-07-14 16:55:52 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,13 +5,13 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var canvas;
-var palyer, playerBase, playerArcher;
+var player, playerBase, playerArcher;
 var baseimage;
 
 function preload() {
-  backgroundImg = loadImage("./assets/background.png");
-  baseimage = loadImage("./assets/base.png");
-  playerimage = loadImage("./assets/player.png");
+  backgroundImg = loadImage('./assets/background.png');
+  baseimage = loadImage('./assets/base.png');
+  playerimage = loadImage('./assets/player.png');
 }
 
 function setup() {
@@ -19,30 +19,35 @@ function setup() {
 
   engine = Engine.create();
   world = engine.world;
+
   angleMode(DEGREES);
 
+  var options = { 
+    isStatic: true
+  }
   //criar corpo da base do jogador
+
+  playerBase = Bodies.rectangle(200, 350, 180, 150, options);
+  World.add(world, playerBase);
 
   //criar corpo do jogador
 
-
-
+  player = Bodies.rectangle(250, playerBase.position.y - 160, 50, 180, options);
+  World.add(world,player);
 }
 
 function draw() {
   background(backgroundImg);
-
   //exibir a imagem do jogador usando a função image()
-
-
   //exibir a imagem da base do jogador usando a função image()
 
-
   Engine.update(engine);
-
+  image(baseimage,playerBase.position.x,playerBase.position.y,180,150)
+  image(playerimage,player.position.x,player.position.y,50,180)
+  
   // Title
-  fill("#FFFF");
-  textAlign("center");
+  fill('#FFFF');
+  textAlign('center');
   textSize(40);
-  text("ARQUEIRO ÉPICO", width / 2, 100);
+  text('ARQUEIRO ÉPICO', width / 2, 100);
 }
